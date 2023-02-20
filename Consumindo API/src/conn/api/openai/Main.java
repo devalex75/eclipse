ec15ae0e37bacae6f9e5bcb67749a2aa52d05a6a
devalex75 <125909486+devalex75@users.noreply.github.com>
2023-02-20 14:31:29 -0300

    Adicionei Arquivos Via Upload

---
+++ Consumindo API/src/conn/api/openai/Main.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package conn.api.openai;
+
+/**
+ * @author alexandre
+ *
+ *	Exemplo uso com GITHUB
+ *
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+	 
+		
+		System.out.println("Hello GitHub");
+	
+		
+
+	}
+
+}
